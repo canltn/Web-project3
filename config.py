bd5da90269522ad6fc8d1582130ba555e9ff4ad6
canltn <lethingoccanst@gmail.com>
2023-11-25 00:11:15 +0700

    message

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=os.getenv('POSTGRES_URL')
-    POSTGRES_USER=os.getenv('POSTGRES_USER')
-    POSTGRES_PW=os.getenv('POSTGRES_PW')
-    POSTGRES_DB=os.getenv('POSTGRES_DB')
+    POSTGRES_URL="canltn-db-server.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="canltn@canltn-db-server" #TODO: Update value
+    POSTGRES_PW="Can12345678"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING =os.getenv('SERVICE_BUS_CONNECTION_STRING')
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://canltn.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=t+jj9+UOhfOTkrqo5KYJpwxEl4AHRhDDk+ASbNqIt/k=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
